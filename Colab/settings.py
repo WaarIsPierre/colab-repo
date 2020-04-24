1d0c49ac8cd7adbdcea9e51317561c527d8cc77b
WaarIsPierre <pierremj12@gmail.com>
2020-04-24 03:42:02 +0200

    Update

--- Colab/settings.py
+++ Colab/settings.py
@@ -119,11 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-# # Put strings here, like "/home/html/static" or "C:/www/django/static".
-# # Always use forward slashes, even on Windows.
-# # Don't forget to use absolute paths, not relative paths.
-# 'projects/static',
-# )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
