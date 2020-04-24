c0908aeed22a894fa788f371718f377711d040e7
WaarIsPierre <pierremj12@gmail.com>
2020-04-24 03:24:41 +0200

    Update

--- Colab/settings.py
+++ Colab/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'projects',
+    'projects',
     # 'blog',
 ]
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Colab.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": ["Colab/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
