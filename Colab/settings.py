7efa6def2752061f913d96bf877cec7aac12ae54
WaarIsPierre <pierremj12@gmail.com>
2020-04-24 03:33:48 +0200

    Update

--- Colab/settings.py
+++ Colab/settings.py
@@ -124,6 +124,6 @@ STATICFILES_DIRS = (
 # Put strings here, like "/home/html/static" or "C:/www/django/static".
 # Always use forward slashes, even on Windows.
 # Don't forget to use absolute paths, not relative paths.
-
+ '/projects/static/'
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
